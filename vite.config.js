e9979f84a06c0fd13a4aab8957785fde555e2fd7
Ethan Wu <405417048@gms.tku.edu.tw>
2024-04-17 16:27:14 +0800

    change to vite

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+    server: {
+        open: '/index.html'
+    },
+    plugins: [react()],
+    esbuild: {
+        loader: "jsx",
+        include: /src\/.*\.jsx?$/,
+        exclude: [],  
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            loader: {
+                '.js': 'jsx', 
+              },
+        },
+    },
+  base: '/migrate-cra-to-vite/'
+})
